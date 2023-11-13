bf02b5020326a11ef2494b558923d88973fcddf9
mrKungISR <150652365+mrKungISR@users.noreply.github.com>
2023-11-13 08:57:38 +0200

    First upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const mrKung = {
+    dangerLevel: 99,
+    style: "Kung Fu",
+    wins: 7856387462918501298473859,
+    losses: 0
+}
+
+document.getElementById("mr-kung").innerHTML = `<p>Danger Level: ${mrKung.dangerLevel} </p>`
